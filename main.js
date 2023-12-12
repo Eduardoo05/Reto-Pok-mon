e2760887201cc322ded43d178e02922e5fd7a695
Elvin Eduardo <franco193@hotmail.com>
2023-12-11 23:22:48 -0400

    se aplico funcionalidad al input

--- main.js
+++ main.js
@@ -1,17 +1,28 @@
 document.querySelector('#app').innerHTML = `
-<h1> hola </h1>
+
+<form action="formulario">
+<div class="">
+  <input class="input_buscar" id="input_buscar" type="text" placeholder="Pokename">
+  <input class="input_botton" id="input_botton" type="button" style="height: 20px; width: 50px;" value="Botón">
+</div>
+</form>
 
 `
-const getCharacters = async () => {
-  const response = await fetch('https://pokeapi.co')
+const getCharacters = async (nombre) => {
+  const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${nombre}/`)
   const data = await response.json()
-  console.log(datos.results)
+  console.log(data)
 
-  data.results.map((character) => {
-    console.log(character.name)
-    console.log(character.status)
-  })
 }
 
-getCharacters()
+
+const input_buscar = document.getElementById('input_buscar')
+const input_botton = document.getElementById('input_botton')
+
+input_buscar.addEventListener('change', (event)=> {
+  event.preventDefault()
+  let nombre_pokemon = input_buscar.value
+  console.log(input_buscar.value)
+  getCharacters(nombre_pokemon)
+})
 
